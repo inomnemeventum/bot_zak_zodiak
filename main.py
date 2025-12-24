13a9992b684ad32e594cd2fe5a4c1f68f28c8154
BasicEC <zak.vitaly@ya.ru>
2025-12-24 22:03:53 +0300

    add some exception handling

--- main.py
+++ main.py
@@ -141,4 +141,11 @@ if __name__ == "__main__":
     print("Запускаю бот...")
     total, _ = load_user_count()
     print(f"Текущее количество пользователей: {total}")
-    bot.infinity_polling(logger_level=logging.INFO)
+    try:
+        bot.infinity_polling(logger_level=logging.INFO)
+        break
+    except ReadTimeout as timeoutError:
+            print(f"Ошибка при работе бота: {timeoutError}")
+    except Exception as e:
+            print(f"Ошибка при работе бота: {e}")
+        
